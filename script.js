b601c09bdc946070343291d66e3d2f8120add0cd
shinzuu <not.shinzuu@gmail.com>
2024-05-29 21:27:41 +0600

    added mld ct2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function updateExamsTime() {
     var exams = [
         { name: "ALGOL perf test(postponed)", time: new Date("2024-06-02T11:30:00") },
         { name: "BIO CT2( ECG [page no. 9-27] )", time: new Date("2024-06-02T08:45:00") },
+        { name: "MLD CT2", time: new Date("2024-06-05T09:30:00") },
         { name: "MID start", time: new Date("2024-06-08T10:30:00") },
         { name: "Final start", time: new Date("2024-08-03T10:30:00") }
     ];
