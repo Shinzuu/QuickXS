8214b6b54a342fcbeef29768d0048454906494e8
shinzuu <not.shinzuu@gmail.com>
2024-11-16 20:03:07 +0600

    events section

--- main-project/src/App.svelte
+++ main-project/src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import Routine from './routes/Routine.svelte';
-  // @ts-ignore
-  import Events from './routes/Events.svelte';  // Import the new Events component
+  import Events from './routes/Events.svelte';
+  import Links from './routes/Links.svelte';  
   let currentRoute = 'home';
 
   function navigate(route) {
@@ -16,19 +16,22 @@
     <div>
       <button on:click={() => navigate('home')} class="mr-4 text-tea_green-100">Home</button>
       <button on:click={() => navigate('routine')} class="mr-4 text-tea_green-100">Routine</button>
-      <button on:click={() => navigate('events')} class="text-tea_green-100">Events</button>
+      <button on:click={() => navigate('events')} class="mr-4 text-tea_green-100">Events</button>
+      <button on:click={() => navigate('links')} class="text-tea_green-100">Links</button>
     </div>
   </nav>
 
   <!-- Main Content -->
   <main class="p-4 bg-beige text-tea_green-100">
     {#if currentRoute === 'home'}
-      <h1 class="text-2xl font-bold text-tea_green-300">next classes</h1>
-      <p class="text-buff-500">upcoming events</p>
+      <h1 class="text-2xl font-bold text-tea_green-300">Next Classes</h1>
+      <p class="text-buff-500">Upcoming Events</p>
     {:else if currentRoute === 'routine'}
       <Routine />
     {:else if currentRoute === 'events'}
       <Events />
+    {:else if currentRoute === 'links'}
+      <Links />
     {/if}
   </main>
 </div>
